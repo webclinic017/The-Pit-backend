7b8fbde19c8ee9fa1ffb389c1ce45a9ff6ad0221
nicholasdegrate <nicholasdegrate@gmail.com>
2021-05-01 17:54:57 -0500

    fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'bob', email: 'bob@gmail.com', password: '123')
+User.create(name: 'nick', email: 'nick@gmail.com', password: '123')
+User.create(name: 'pesh', email: 'pesh@gmail.com', password: '123')
